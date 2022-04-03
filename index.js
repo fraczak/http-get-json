40fb369e583603c572c68f510b97aaa407da0949
Wojciech Fraczak <fraczak@gmail.com>
2022-04-02 22:54:12 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var http = require("http");
+
+var httpGetJSON = (url, cb) => {
+  http.get(url, res => {
+    let result = [];
+    res.setEncoding('utf8')
+    .on('data', (chunk) => result.push(chunk))
+    .on('end', () => {
+      try { 
+        cb(null, JSON.parse(result.join('')));
+      } catch (err) { cb(err); }
+    })
+  }).on('error', cb);
+};
+
+module.exports = httpGetJSON;
