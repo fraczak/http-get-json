d50ff584e3124295427de1724b0a92bbabe5188a
Wojciech Fraczak <fraczak@gmail.com>
2022-04-02 23:26:53 -0400

    made it return <http.ClientRequest> object

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var http = require("http");
 
-var httpGetJSON = (url, cb) => {
-  http.get(url, res => {
-    var result = [];
-    res.setEncoding('utf8')
-    .on('data', (chunk) => result.push(chunk))
+var httpGetJSON = ( url, cb ) =>
+  http.get( url, res => ( chunks =>
+    res.setEncoding( "utf8" )
+    .on('data', chunk => chunks.push(chunk))
     .on('end', () => {
       try { 
-        cb(null, JSON.parse(result.join('')));
+        cb(null, JSON.parse( chunks.join('') ));
       } catch (err) { cb(err); }
-    })
-  }).on('error', cb);
-};
+    }))([]))
+  .on('error', cb);
 
 module.exports = httpGetJSON;
